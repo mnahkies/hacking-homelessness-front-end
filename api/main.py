bbc02defde5dc1a8efe1c7cd7e872b05af73d19f
Christopher DeChong Yang <cdcyang@gmail.com>
2019-02-23 23:51:03 +0000

    app engine deployment.

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from . import mock_db
+from start import mock_db
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -18,9 +18,7 @@ def get_achievement():
 @app.route('/questions')
 def get_question():
     data = mock_db.get('question', dict())
-    response = dict()
-    for i in range(len(data)):
-        response[str(i+1)] = data[i]
+    response = _get_kvp_response(data)
     return jsonify(response)
 
 
@@ -29,5 +27,23 @@ def get_language():
     return jsonify(mock_db.get('language', dict()))
 
 
+@app.route('/lesson/bank')
+def get_lesson():
+    lessons = mock_db.get('lessons', dict())
+    if not lessons:
+        return jsonify(dict())
+
+    data = lessons.get("bank", dict())
+    response = _get_kvp_response(data)
+    return jsonify(response)
+
+
+def _get_kvp_response(data):
+    response = dict()
+    for i in range(len(data)):
+        response[str(i + 1)] = data[i]
+    return response
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=False)
