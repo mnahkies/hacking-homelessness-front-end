6226b32dd78cacc6e84ac038bb301eaa55055f27
gunnerr91 <blissvortex@hotmail.com>
2019-02-23 20:51:13 +0000

    nav bar finished

--- src/app/components/nav-header/nav-header.component.ts
+++ src/app/components/nav-header/nav-header.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-header',
-  templateUrl: './nav-header.component.html',
-  styleUrls: ['./nav-header.component.scss'],
+  selector: "app-nav-header",
+  templateUrl: "./nav-header.component.html",
+  styleUrls: ["./nav-header.component.scss"]
 })
 export class NavHeaderComponent implements OnInit {
-
-  constructor() {
-  }
+  points: string = "0";
+  constructor() {}
 
   ngOnInit() {
+    this.points = "240";
   }
-
 }
