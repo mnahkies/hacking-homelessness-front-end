c02120c517e1fcdeb867d69a8f891dc41f34f20c
gunnerr91 <blissvortex@hotmail.com>
2019-02-24 13:26:06 +0000

    removed points from lesson converstaion

--- src/app/pages/lesson-page/lesson-page.component.ts
+++ src/app/pages/lesson-page/lesson-page.component.ts
@@ -1,27 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {ConversationScript, ConversationScriptResult} from '../../interfaces';
-import {Subscription} from 'rxjs';
-import {Store} from '@ngxs/store';
-import {CoreState} from '../../state/core.state';
-import {IncreaseScore} from '../../state/core.actions';
+import { Component, OnInit } from "@angular/core";
+import { ConversationScript, ConversationScriptResult } from "../../interfaces";
+import { Subscription } from "rxjs";
+import { Store } from "@ngxs/store";
+import { CoreState } from "../../state/core.state";
+import { IncreaseScore } from "../../state/core.actions";
 
 @Component({
-  selector: 'app-lesson-page',
-  templateUrl: './lesson-page.component.html',
-  styleUrls: ['./lesson-page.component.scss'],
+  selector: "app-lesson-page",
+  templateUrl: "./lesson-page.component.html",
+  styleUrls: ["./lesson-page.component.scss"]
 })
 export class LessonPageComponent implements OnInit {
-
   conversationScript?: ConversationScript;
 
   private subscription = Subscription.EMPTY;
 
-  constructor(readonly store: Store) {
-  }
+  constructor(readonly store: Store) {}
 
   ngOnInit() {
-    this.subscription = this.store.select(CoreState.getFinanceLessonConversationScript)
-      .subscribe(it => this.conversationScript = it);
+    this.subscription = this.store
+      .select(CoreState.getFinanceLessonConversationScript)
+      .subscribe(it => (this.conversationScript = it));
   }
 
   ngOnDestroy(): void {
@@ -29,8 +28,8 @@ export class LessonPageComponent implements OnInit {
   }
 
   onScriptCompleted(result: ConversationScriptResult) {
-    console.info('conversation completed with result', result);
+    console.info("conversation completed with result", result);
     // todo persist
-    this.store.dispatch(new IncreaseScore(20))
-  };
+    this.store.dispatch(new IncreaseScore(0));
+  }
 }
