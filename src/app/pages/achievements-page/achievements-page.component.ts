b3b67d5659e8d6f754803d68acf8151b4c7b343a
gunnerr91 <blissvortex@hotmail.com>
2019-02-24 13:59:04 +0000

    achievement points updated

--- src/app/pages/achievements-page/achievements-page.component.ts
+++ src/app/pages/achievements-page/achievements-page.component.ts
@@ -1,43 +1,65 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngxs/store";
+import { CoreState } from "src/app/state/core.state";
 
 interface Achievement {
-  points: number
-  category: string
+  points: number;
+  category: string;
 }
 
 interface Streak {
-  headerColor: string
-  title: string
-  message: string
+  headerColor: string;
+  title: string;
+  message: string;
 }
 
 @Component({
-  selector: 'app-achievements-page',
-  templateUrl: './achievements-page.component.html',
-  styleUrls: ['./achievements-page.component.scss'],
+  selector: "app-achievements-page",
+  templateUrl: "./achievements-page.component.html",
+  styleUrls: ["./achievements-page.component.scss"]
 })
 export class AchievementsPageComponent implements OnInit {
   achievements: Achievement[] = [
-    {category: 'Managing Money', points: 20},
-    {category: 'Health and wellbeing', points: 0},
-    {category: 'Life skills', points: 10},
-    {category: 'Education and school', points: 0},
-    {category: 'Getting a job', points: 0},
-    {category: 'Housing and rent', points: 0},
+    { category: "Managing Money", points: 20 },
+    { category: "Health and wellbeing", points: 0 },
+    { category: "Life skills", points: 0 },
+    { category: "Education and school", points: 0 },
+    { category: "Getting a job", points: 0 },
+    { category: "Housing and rent", points: 0 }
   ];
-  achievementsColumns = ['category', 'points'];
+  achievementsColumns = ["category", "points"];
 
   streaks: Streak[] = [
-    {headerColor: 'streak-yellow', title: 'Topic Streak', message: '3 topics done in a day!'},
-    {headerColor: 'streak-peach', title: 'Daily Streak', message: 'Learning 3 days in a row'},
-    {headerColor: 'streak-yellow', title: 'Something Streak', message: '10 somethings done in a day!'},
-    {headerColor: 'streak-peach', title: 'Yearly Streak', message: 'Learning 2 years in a row'},
+    {
+      headerColor: "streak-yellow",
+      title: "Topic Streak",
+      message: "3 topics done in a day!"
+    },
+    {
+      headerColor: "streak-peach",
+      title: "Daily Streak",
+      message: "Learning 3 days in a row"
+    },
+    {
+      headerColor: "streak-yellow",
+      title: "Something Streak",
+      message: "10 somethings done in a day!"
+    },
+    {
+      headerColor: "streak-peach",
+      title: "Yearly Streak",
+      message: "Learning 2 years in a row"
+    }
   ];
 
-  constructor() {
+  points: number = 0;
+  constructor(store: Store) {
+    store
+      .select(CoreState.getScore)
+      .subscribe({ next: v => (this.points = v) });
   }
 
   ngOnInit() {
+    this.achievements[0].points = this.points;
   }
-
 }
